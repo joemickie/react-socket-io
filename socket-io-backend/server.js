27eab4b8056580bcb36266d29f0b5da3104f1c31
Chukwuemeka Chinemelu <wikiwoo@MacBook-Air.local>
2024-05-04 16:23:38 +0100

    first commit

---
+++ socket-io-backend/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
+const socketIo = require('socket.io');
+const { clear } = require('console');
+
+const app = express();
+app.use(cors())
+const server = http.createServer(app);
+
+const io = socketIo(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+    }
+})
+
+let lastPrice = Math.random() * 100 + 50; // Random starting price between $50 and $150
+let day = 0;
+
+io.on('connection', (socket) => {
+
+    const interval = setInterval(() => {
+        const changePercent = 2 * Math.random();
+        let changeAmount = lastPrice * (changePercent / 100);
+        changeAmount *= Math.floor(Math.random() * 2) === 1 ? 1 : -1;
+        lastPrice += changeAmount;
+
+        // Emit updated stock data with an increasing day count
+        socket.emit('stockData', { price: lastPrice, day: ++day });
+    }, 2000);
+
+    socket.on('disconnect', () => {
+        clearInterval(interval);
+    });
+})
+
+const PORT = 4000;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ socket-io-frontend/src/App.js
@@ -0,0 +1,63 @@
+import './App.css';
+import io from "socket.io-client";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import { useEffect, useState } from 'react';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+)
+
+const socket = io("http://localhost:4000");
+// const socket = io("https://client-server-single-app-backend.onrender.com")
+
+function App() {
+  return (
+    <div className="App">
+      <RealtimeChart />
+    </div>
+  );
+}
+
+const RealtimeChart = () => {
+  const [dataPoints, setDataPoints] = useState([]);
+
+  useEffect(() => {
+    socket.on("stockData", (data) => {
+      setDataPoints(currentPoints => [...currentPoints, data])
+    })
+  }, [])
+
+  const chartData = {
+    labels: dataPoints.map(point => point.day),
+    datasets: [
+      {
+        label: 'Stock Price',
+        data: dataPoints.map(point => point.price),
+        fill: false,
+        borderColor: 'rgba(75, 192, 192, 1)'
+      },
+    ],
+  };
+
+  return (<div style={{width: '70%', height: '70vh', margin: '100px auto'}}>
+    <Line data={chartData}/>
+  </div>)
+}
+
+export default App;
